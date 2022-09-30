a63672e47f36a74101ace3fab32b4f09e53d12c7
Logan Nguyen <lg.131.dev@gmail.com>
2022-09-29 23:14:25 -0400

    App cleanup && added comments

--- agent/initializers/loadEnvVars.go
+++ agent/initializers/loadEnvVars.go
@@ -1,11 +1,23 @@
+/*
+	@Author: Logan (Nam) Nguyen
+	@Course: SUNY Oswego - CSC 482
+	@Instructor: Professor James Early
+	@Purpose: loadEnvVars.go provide function that helps load environemnt variables
+*/
+
+/* @package initializers */
 package initializers
 
+// import packages
 import (
 	"log"
 
 	"github.com/joho/godotenv"
 )
 
+/*
+	@Function LoadEnvVars() - loads environment varables
+*/
 func LoadEnvVars()  {
 	err := godotenv.Load()
   	if err != nil {
--- agent/main.go
+++ agent/main.go
@@ -1,5 +1,13 @@
+/*
+	@Author: Logan (Nam) Nguyen
+	@Course: SUNY Oswego - CSC 482
+	@Instructor: Professor James Early
+*/
+
+// @package: main
 package main
 
+// Import packages
 import (
 	"NFTir/agent/initializers"
 	"log"
@@ -7,15 +15,19 @@ import (
 	"github.com/jamespearly/loggly"
 )
 
+// initialize global variables
 var (
 	logglyClient *loggly.ClientType
 )
 
+/* @function init() - run before main() */
 func init()  {
 	initializers.LoadEnvVars();
 	logglyClient = loggly.New("NFTir")
 }
 
+
+/* @function main() - root function */
 func main()  {
 	
 	log.Println("NFTir")
