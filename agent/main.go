2c64312dde31698fe4b376766f5bd79d4cedd37e
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-13 11:21:07 -0400

    Establish dynamodb connection in main.go

--- agent/main.go
+++ agent/main.go
@@ -9,26 +9,30 @@ package main
 
 // Import packages
 import (
+	"NFTir/agent/controllers"
 	"NFTir/agent/initializers"
+	"NFTir/agent/utils"
 
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/jamespearly/loggly"
 )
 
 // initialize global variables
 var (
-	logglyClient *loggly.ClientType
+	logglyClient *loggly.ClientType // loggyClient := jearly/loggly
+	tableName string
+	db *dynamodb.DynamoDB
 )
 
 /* @function init() - run before main() */
 func init()  {
 	initializers.LoadEnvVars();
-	
 	logglyClient = loggly.New("NFTir")
+	tableName = "nnguyen6_NFTir_v1"
+	db = utils.EstablishAwsDynamodbSession()
 }
-
 
 /* @function main() - root function */
 func main()  {
-
-	// services.PeriodicallyFetchData(logglyClient)
+	controllers.PeriodicallyFetchData(logglyClient, tableName, db);
 }
