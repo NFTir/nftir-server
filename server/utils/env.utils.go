2c7b596ad8cfdae68e5ef564aef8c401a749dc91
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-19 18:53:57 -0400

    Create env.utils.go

---
+++ server/utils/env.utils.go
@@ -0,0 +1,26 @@
+/*
+	@Author: Logan (Nam) Nguyen
+	@Course: SUNY Oswego - CSC 482
+	@Instructor: Professor James Early
+	@Purpose: env.utils.go provides function that helps load environemnt variables
+*/
+
+/* @package initializers */
+package utils
+
+// import packages
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+/*
+@func: LoadEnvVars() - loads environment varables
+*/
+func LoadEnvVars()  {
+	err := godotenv.Load()
+  	if err != nil {
+    	log.Fatal("Error loading .env file")
+  	}
+}
