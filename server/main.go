f1917c271da890d73cae79bbf653866353da08d1
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-19 18:54:04 -0400

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1,17 +1,36 @@
+/*
+	@Author: Logan (Nam) Nguyen
+	@Course: SUNY Oswego - CSC 482
+	@Instructor: Professor James Early
+*/
+
+// @package: main
 package main
 
+// Import packages
 import (
-	"net/http"
+	"NFTir/server/routers"
+	"NFTir/server/utils"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/jamespearly/loggly"
 )
 
+// initialize global variables
+var (
+	logglyClient *loggly.ClientType // loggyClient := jearly/loggly
+)
+
+/* @func: init() - run before main() */
+func init()  {
+	if (os.Getenv("GIN_MODE") != "release") {
+		utils.LoadEnvVars()
+	}
+	logglyClient = loggly.New("NFTir")
+}
+
+/* @function main() - root function */
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-	  c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	  })
-	})
-	r.Run(":9098")
-  }
+	router := routers.SetupRouter();
+	router.Run("localhost"+os.Getenv("PORT"))
+}
