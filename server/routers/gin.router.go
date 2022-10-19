fa61f4d69521f80d6ba882bd8cfb8efb206735be
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-19 19:47:23 -0400

    Update gin.router.go

--- server/routers/gin.router.go
+++ server/routers/gin.router.go
@@ -5,26 +5,19 @@
 	@Purpose: gin.router.go provides function(s) related to gin's default router
 */
 
+/* @package services */
 package routers
 
+// import packages
 import (
-	"net/http"
+	"NFTir/server/controllers"
 
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter() *gin.Engine {
-	// init gin router
-	router := gin.Default()
-
-	// Gin trust all proxies by default. 192.168.1.2 typically assigned to home routers.
-	router.SetTrustedProxies([]string{"192.168.1.2"})
-
-	// Get request
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-		  "Client IP": c.ClientIP(),
-		})
-	  })
-	return router
+/*
+@func: SetupRouter() - set up RESTful endpoints
+*/
+func SetupRouter(routerGroup *gin.RouterGroup) {
+	routerGroup.GET("/status", controllers.GetStatus)
 }
