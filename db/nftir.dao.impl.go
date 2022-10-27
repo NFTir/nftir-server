a5357d3fab8957f0623c7b9718d532655fd7ca1f
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-27 10:54:19 -0400

    Implemented nftir.dao implementation

---
+++ db/nftir.dao.impl.go
@@ -0,0 +1,65 @@
+/*
+	@Author: Logan (Nam) Nguyen
+	@Course: SUNY Oswego - CSC 482
+	@Instructor: Professor James Early
+	@Purpose: env.utils.go provides function that helps load environemnt variables
+*/
+
+// @package
+package db
+
+// @import
+import (
+	"NFTir/server/models"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+// @notice Holds information related to dynamodb
+type NftirDaoImpl struct {
+	dynamodb *dynamodb.DynamoDB
+}
+
+// @dev constructor
+func NftirDaoConstructor(dynamodb *dynamodb.DynamoDB) NftirDao {
+	return &NftirDaoImpl {
+		dynamodb: dynamodb,
+	}
+}
+
+func (ndi *NftirDaoImpl) GetAll() (*[]models.Collection, error) {
+
+	// Scan the dynamoDB `TABLE_NAME` table
+	collectionSO, err := ndi.dynamodb.Scan(&dynamodb.ScanInput{
+		TableName: aws.String(os.Getenv("TABLE_NAME")),
+	})
+	if err != nil {return nil, err}
+
+	
+	// dynamic slice
+	collections := make([]models.Collection, *collectionSO.Count)
+	
+	// process collectionAV *dynamodb.ScanOutput
+	for index, collectionsAV := range collectionSO.Items {
+		collection := models.Collection{}
+
+		if err = dynamodbattribute.UnmarshalMap(collectionsAV, &collection); err != nil {
+			return nil, err
+		}
+
+		collections[index] = collection
+	}
+
+	return &collections, nil
+}
+
+func (ndi *NftirDaoImpl) GetStatus() (*models.HttpStatusMessage, error) {
+	return nil, nil
+}
+
+func (ndi *NftirDaoImpl) GetCollectionsBy(volumeUsd *float64, marketCapUsd *float64) (*[]models.Collection, error) {
+	return nil, nil;
+}
