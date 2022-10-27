158066111448cf2816bf3fb4300e49f62f89c66b
Logan Nguyen <lg.131.dev@gmail.com>
2022-10-27 10:53:38 -0400

    Updated models

---
+++ models/responses.model.go
@@ -0,0 +1,23 @@
+/*
+	@Author: Logan (Nam) Nguyen
+	@Course: SUNY Oswego - CSC 482
+	@Instructor: Professor James Early
+	@Purpose: logglyMessage.model.go provides LogglyMessage struct to send to loggly
+*/
+
+// @package
+package models
+
+// @notice information realted to message struct to send to loggly
+type HttpLogglyMessage struct {
+	Status_Code uint 	`json:"status_code"`
+	Method_Type string	`json:"method_type"`
+	Source_Ip string 	`json:"source_ip"`
+	Req_Path string 	`json:"req_path"`
+}
+
+// @notice information related to message struct serving GET/status
+type HttpStatusMessage struct {
+	Table string			`json:"table"`
+	Record_Count string		`json:"record_count"`
+}
